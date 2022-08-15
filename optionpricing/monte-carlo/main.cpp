271a8bf1ee205600a6281e935e053f0fe0a0e617
nilesh05apr <nk8588338@gmail.com>
2022-08-15 17:14:14 +0530

    code refactor

--- optionpricing/monte-carlo/main.cpp
+++ optionpricing/monte-carlo/main.cpp
@@ -11,14 +11,27 @@ int main() {
     int n_samples = 5000;
     double seed = time(nullptr);
     double strike = 650.0;                  // strike price of derivative
-    Paytype pt = Paytype::CALL;                      //'Payoff type call or put'
+    Paytype pt = Paytype::CALL;             //'Payoff type call or put'
+    int x;                      
+
+    std::cout<<"---------------------------------------------------------------------"<<std::endl;
+    std::cout<<"Enter Spot price of the asset: ";
+    std::cin>>initPrice;
+    std::cout<<"Enter Time to expiry in days: ";
+    std::cin>>timeperiod;
+    std::cout<<"Enter Volatility in decimals: ";
+    std::cin>>volatility;
+    std::cout<<"Enter Expected return rate in decimals: ";
+    std::cin>>rate;
+    std::cout<<"Enter Strike price of the option: ";
+    std::cin>>strike;
+    std::cout<<"Press 1 for CALL                          Press 0 for PUT";
+    std::cin>>x;
+    x == 1 ? pt = Paytype::CALL : pt = Paytype::PUT;
 
     MonteCarlo mcp = MonteCarlo(timeperiod,volatility,initPrice,rate,strike,pt,n_samples);
     std::cout<<mcp.getOptionPrice()<<std::endl;
 
-    for(auto x: mcp.seeds){
-        std::cout<<x<<" ";
-    }
 
     return 0;
 }
